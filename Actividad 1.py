cd27b398fbe757a06a300b32b9a42b206da9a438
Mateo <matteoromero0320@gmail.com>
2026-03-14 16:40:33 -0300

    Punto 1 al 4 hechos

--- Actividad 1.py
+++ Actividad 1.py
@@ -1,3 +1,41 @@
-name = "Mateo"
-second_name = "Romero"
-print(name + second_name)
+#Actividades introductorias
+
+#1) Escribe un programa que solicite al usuario su año de nacimieto 
+#   y muestre en qué año cumplirá 18, 21, 100 años
+
+
+#edad = int(input("Ingrese su año de nacimiento: "))
+#print(f"Cumplirá 18 en el año {edad + 18}")
+#print(f"Cumplirá 21 en el año {edad + 21}")
+#print(f"Cumplirá 100 en el año {edad + 100}")
+
+#-------------------------------------------------------------------------
+
+#2) Escribe un programa que solicite al usuario una cantidad de segundos
+#   y muestre cuántas horas, minutos y segundo equivalen
+
+#seg_ingresado = int(input("Ingrese una cantidad de segundos: "))
+#horas = seg_ingresado // 3600
+#minutos = (seg_ingresado%3600) // 60 
+#segundos = (seg_ingresado%3600) % 60
+#print(f"{horas} : {minutos} : {segundos}")
+
+#-------------------------------------------------------------------------
+
+#3) 3. Crea un programa que solicite al usuario un número 
+# y muestre su tabla de multiplicar del 1 al 10 utilizando un bucle for.
+
+#multiplo = int(input("Ingrese un numero: "))
+#for num in range(1,11):
+#    print(f"{num} x {multiplo} = {num * multiplo}")
+
+#-------------------------------------------------------------------------
+
+#4) Crea un programa que dado un número N ingresado por el usuario, 
+#   imprima los números del 1 al N pero saltee los múltiplos de 5
+n = int(input("Ingrese un numero: "))
+for num in range(1,(n+1)):
+    if (num % 5) != 0:
+        print(f"{num}", end="-")
+
+#-------------------------------------------------------------------------
